b0a49566e0e85d0aa35ed81ec04ad961b7ca8ed6
Andrew Lazarus <nerdrew@gmail.com>
2017-05-16 22:40:57 -0700

    rspec3

--- spec/zk/client/caching_spec.rb
+++ spec/zk/client/caching_spec.rb
@@ -20,16 +20,16 @@ describe ZK::Client::Caching, zookeeper: true do
   end
 
   it "registers paths and listens for updates" do
-    zkcc["/test/boom"].should == "goat"
-    zkcc["/test/foo"].should == 1
+    expect(zkcc["/test/boom"]).to eq("goat")
+    expect(zkcc["/test/foo"]).to eq(1)
 
     zk.create("/test/boom")
     zk.set("/test/boom", "cat")
-    almost_there { zkcc["/test/boom"].should == "cat" }
+    almost_there { expect(zkcc["/test/boom"]).to eq("cat") }
 
     zk.create("/test/foo")
     zk.set("/test/foo", "1")
-    almost_there { zkcc["/test/foo"].should == 2 }
+    almost_there { expect(zkcc["/test/foo"]).to eq(2) }
   end
 
   describe "flakey connections", proxy: true do
@@ -37,32 +37,32 @@ describe ZK::Client::Caching, zookeeper: true do
 
     context "with slow connections", throttle_bytes_per_sec: 100 do
       it "works" do
-        zkcc["/test/boom"].should == "goat"
-        zkcc["/test/foo"].should == 1
+        expect(zkcc["/test/boom"]).to eq("goat")
+        expect(zkcc["/test/foo"]).to eq(1)
 
         zk.create("/test/boom")
         zk.set("/test/boom", "cat")
-        almost_there { zkcc["/test/boom"].should == "cat" }
+        almost_there { expect(zkcc["/test/boom"]).to eq("cat") }
 
         zk.create("/test/foo")
         zk.set("/test/foo", "1")
-        almost_there { zkcc["/test/foo"].should == 2 }
+        almost_there { expect(zkcc["/test/foo"]).to eq(2) }
       end
     end
 
     it "works with dropped connections" do
-      zkcc["/test/boom"].should == "goat"
+      expect(zkcc["/test/boom"]).to eq("goat")
       zk.create("/test/boom")
       zk.set("/test/boom", "cat")
-      almost_there { zkcc["/test/boom"].should == "cat" }
+      almost_there { expect(zkcc["/test/boom"]).to eq("cat") }
       proxy_stop
       sleep 1
       zk.set("/test/boom", "dog")
-      zkcc["/test/boom"].should == "cat"
+      expect(zkcc["/test/boom"]).to eq("cat")
       sleep 12
       proxy_start
       sleep 1
-      almost_there { zkcc["/test/boom"].should == "dog" }
+      almost_there { expect(zkcc["/test/boom"]).to eq("dog") }
     end
   end
 end
